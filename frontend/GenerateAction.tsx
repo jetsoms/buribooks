1e38740a60149cee4e9d646dd63889c37ee44ae6
John Soriano <iam@johnsoriano.dev>
2022-06-27 12:06:48 +0800

    Updated

--- frontend/GenerateAction.tsx
+++ frontend/GenerateAction.tsx
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { Button, useBase, Loader } from "@airtable/blocks/ui";
+import {
+  Button,
+  useBase,
+  Loader,
+  FormField,
+  Input,
+  Text,
+} from "@airtable/blocks/ui";
 
 export interface GenerateActionProps {
   bookTitle: string;
@@ -12,33 +19,57 @@ export const GenerateAction: React.FC<GenerateActionProps> = ({
   bookRecordId,
   batchRecordId,
 }): React.ReactElement => {
+  const [created, setCreated] = useState(0);
+  const [count, setCount] = useState(0);
   const [loading, setLoading] = useState(false);
   const base = useBase();
   const handleGenerate = async () => {
     setLoading(true);
     try {
       const table = base.getTableByName("Cards");
-      const records = [...Array(50).keys()].map((_) => ({
+      const records = [...Array(count).keys()].map((_) => ({
         fields: {
           "Book ID": bookRecordId,
           "Book Title": bookTitle,
           Batch: [{ id: batchRecordId }],
         },
       }));
-      await table.createRecordsAsync(records);
+      const recordCreated = await table.createRecordsAsync(records);
+      setCreated(recordCreated.length);
       setLoading(false);
     } catch (err) {
       setLoading(false);
     }
   };
 
+  const handleCountChange = (value: string) => {
+    const count = parseInt(value, 10);
+    setCount(count > 50 ? 50 : count);
+  };
   return (
     bookRecordId &&
     bookTitle && (
-      <Button disabled={loading} onClick={handleGenerate} icon="bolt">
-        {`Generate (50) Cards for ${bookTitle}`}{" "}
-        {loading && <Loader scale={0.3} />}
-      </Button>
+      <React.Fragment>
+        <FormField label="How many cards?">
+          <Input
+            value={String(count)}
+            type="number"
+            onChange={(e) => handleCountChange(e.target.value)}
+            max={50}
+          />
+        </FormField>
+        {count > 0 && (
+          <Button disabled={loading} onClick={handleGenerate} icon="bolt">
+            {`Generate (${count}) Cards for ${bookTitle}`}{" "}
+            {loading && <Loader scale={0.3} />}
+          </Button>
+        )}
+        {created > 0 && (
+          <Text
+            marginY={2}
+          >{`${created} cards successfully, please add QR Code`}</Text>
+        )}
+      </React.Fragment>
     )
   );
 };
